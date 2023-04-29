ac98784a695c39c142972f14de73fc9321259c2c
bnulens <brechtnulens@gmail.com>
2023-04-29 18:03:04 +0200

    feat - Include game countdown counter - Describe functions

--- game.js
+++ game.js
@@ -1,5 +1,20 @@
-import { interval } from "rxjs";
-// Game countdown
-let gameCountDown$ = interval(3000);
+import { interval, take } from "rxjs";
 
-gameCountDown$.subscribe(console.log);
+let gameGrid = document.getElementById("game-grid");
+gameGrid.style.visibility = "hidden";
+
+// Display grid after countdown
+// Game countdown
+interval(1000)
+.pipe(take(4))
+.subscribe({ next: console.log, complete: () => console.log("Completed !") });
+
+// Determine which player goes first + Start turn on said player
+// Start countdown on player
+// Determine a move 
+
+// If move is made
+// a) check legality move
+// b) register move to object
+// b) switch turn to other player
+
