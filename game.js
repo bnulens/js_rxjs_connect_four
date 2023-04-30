c602f2893019b314c27410ea37411c498e51c166
bnulens <brechtnulens@gmail.com>
2023-04-30 18:28:45 +0200

    feat - Fixed countdown stream - Import gameGridArray

--- game.js
+++ game.js
@@ -1,20 +1,49 @@
-import { interval, take } from "rxjs";
+import { gameGridArray } from "./grid";
+import { interval } from "rxjs";
+import { scan, map, tap, takeWhile } from "rxjs/operators";
 
-let gameGrid = document.getElementById("game-grid");
-gameGrid.style.visibility = "hidden";
-
+console.log("game logic")
+console.log(gameGridArray);
 // Display grid after countdown
+
 // Game countdown
-interval(1000)
-.pipe(take(4))
-.subscribe({ next: console.log, complete: () => console.log("Completed !") });
+// Stream
+// const counter$ = interval(1000);
+
+// counter$
+//   .pipe(
+//     map(() => -1),
+//     scan((accu, curr) => {
+//       return accu + curr;
+//     }, 4),
+//     // tap(console.log)
+//     takeWhile((value) => value >= 0)
+//     )
+//   .subscribe((value) => {
+//     console.log(value)
+//     if (!value) {
+//         console.log("Start !")
+//     }
+// });
 
 // Determine which player goes first + Start turn on said player
-// Start countdown on player
-// Determine a move 
 
-// If move is made
-// a) check legality move
-// b) register move to object
-// b) switch turn to other player
+// Start countdown on player
+// Each player has a set amount of time to try and win the game (chess clock)
+
+/*
+  --- Determining a move ---
+  1. Moves can only be made in columns
+  2. A click on a column adds a player's coin to the array starting from 0 (zero-based)
+  3. A column can get full => disable the column
+  4. Determine legality
+*/  
+
+/*
+  --- Determining legal moves ---
+  1. Is column array full ?
+  2. Is there a winning combination => continue or end game
+  3. 
+*/
+
 
