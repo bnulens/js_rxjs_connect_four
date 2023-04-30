695bb210e241b2744fc878df5ce433b4050346c7
bnulens <brechtnulens@gmail.com>
2023-04-30 22:59:59 +0200

    feat - Determine playerTurn after gameCountDown pass

--- game.js
+++ game.js
@@ -2,31 +2,39 @@ import { gameGridArray } from "./grid";
 import { interval } from "rxjs";
 import { scan, map, tap, takeWhile } from "rxjs/operators";
 
-console.log("game logic")
-console.log(gameGridArray);
-// Display grid after countdown
+// Determine which player goes first + Start turn on said player
+let player1Turn;
+let player2Turn;
 
+// Display grid after countdown
 // Game countdown
 // Stream
-// const counter$ = interval(1000);
-
-// counter$
-//   .pipe(
-//     map(() => -1),
-//     scan((accu, curr) => {
-//       return accu + curr;
-//     }, 4),
-//     // tap(console.log)
-//     takeWhile((value) => value >= 0)
-//     )
-//   .subscribe((value) => {
-//     console.log(value)
-//     if (!value) {
-//         console.log("Start !")
-//     }
-// });
-
-// Determine which player goes first + Start turn on said player
+const counter$ = interval(1000);
+counter$
+  .pipe(
+    map(() => -1),
+    scan((accu, curr) => {
+      return accu + curr;
+    }, 4),
+    // tap(console.log)
+    takeWhile((value) => value >= 0)
+    )
+  .subscribe((value) => {
+    console.log(value)
+    if (!value) {
+        console.log("Start !")
+        // Determining who gets to start the game
+        let starterChance = Math.floor(Math.random() * 2);
+        if (starterChance == 0) {
+          player1Turn = true;
+          player2Turn = false
+        } else {
+          player1Turn = false
+          player2Turn = true
+        }
+        console.log("p1",player1Turn, "p2",player2Turn)
+    }
+});
 
 // Start countdown on player
 // Each player has a set amount of time to try and win the game (chess clock)
