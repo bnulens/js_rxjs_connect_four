b57a5efba937d3d10e1446629fc8d9e99febd003
bnulens <brechtnulens@gmail.com>
2023-04-30 18:29:52 +0200

    feat - Generate grid + export gameGridArray as reference

--- grid.js
+++ grid.js
@@ -1,4 +1,4 @@
-// We can load the external script anywhere we want 
+// We can load the external script after the grid has been built
 async function loadGameLogic() {
     try {
         await import('./game.js');
@@ -8,33 +8,56 @@ async function loadGameLogic() {
     }
 };
 
+// Dynamically generate a grid after 'Start game' is clicked
+let gameMountingPoint = document.getElementById("game-mounting-point")
+let newGrid = document.createElement("ul");
+newGrid.classList.add('game-grid')
+newGrid.setAttribute('id', 'game-grid');
+gameMountingPoint.appendChild(newGrid);
+
+// gameGridArray is an array of arrays referencing all the positions on the grid
+let gameGridArray = [];
+// gameGrid is the HTML grid consisting of unordered lists
 let gameGrid = document.getElementById('game-grid');
 
 // This function generates a grid depending on the preferred size
+// This function also pushes references to the positions into gameGridArray
 const makeGridFromSessionStorage = (gridSizeString) => {
-    // Here we get the columns and rows for the grid in string
-    let [gridColumns, gridRows] = gridSizeString.split('x');
-    // gridX-Y are numbers used to define the amount of tiles to build the grid 
-    let [gridX, gridY] = gridSizeString.split('x').map(num => +num);
-    
-    gameGrid.classList.add(`size-${gridColumns}-${gridRows}`);
-    
-    for (let i = 0; i < gridX; i++) {
-        let gridColumnList = document.createElement("ul");
-        gameGrid.appendChild(gridColumnList);
+    if (!gridSizeString) {
+        console.error('error')
+    } else {
+        // Getting the columns and rows for the grid in string
+        // let [gridColumns, gridRows] = gridSizeString.split('x');
 
-        for (let j = 0; j < gridY; j++) {
-            let gameGridTile = document.createElement('li');
-            // Tiles are not zero-based !
-            gameGridTile.setAttribute("value",j + 1);
-            gridColumnList.appendChild(gameGridTile)
+        // gridX-Y are numbers used to define the amount of tiles to build the grid 
+        let [gridX, gridY] = gridSizeString.split('x').map(num => +num);
+        
+        // Each column has it's own array
+        for (let i = 0; i < gridX; i++) {
+            let gridColumnArray = [];
+            // Each column is an unordered list
+            let gridColumn = document.createElement("ul");
+
+            // The height of the grid is defined by gridY
+            for (let j = 0; j < gridY; j++) {
+                // Generating gridY times list elements 
+                let gridColumnItem = document.createElement("li");
+                gridColumnItem.classList.add(`${j}`);
+                // Appending every list item to gridColumn
+                gridColumn.appendChild(gridColumnItem);
+
+                // Push every list item into gridColumnArray for future reference
+                gridColumnArray.push(gridColumnItem);
+            }
+            gameGrid.appendChild(gridColumn);
+            gameGridArray.push(gridColumnArray);
         }
+        return gameGridArray
     }
-
-    // Load game logic when grid is built
-    loadGameLogic();
 }
 
 makeGridFromSessionStorage(window.sessionStorage.getItem('grid-size'));
+// Load game logic when grid is built
+loadGameLogic();
 
-  
+export { gameGridArray };
